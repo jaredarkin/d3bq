a100d57118e10b35d45eb5f1f150984d95ee21de
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-09-18 13:20:26 -0400

    updated heroku connection

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,13 @@
 var mongojs = require("mongojs")
 
-var dbUrl = "d3bq"
-var collections = ["barbecues"]
+// var dbUrl = "d3bq"
+// var collections = ["barbecues"]
 
-var db = mongojs(dbUrl, collections)
+var mongoURI = process.env.MONGOLAB_URI;
+var localURI = 'mongodb://localhost/barbecues';
+var db = mongojs( mongoURI || localURI, ['barbecues']);
+
+
+// var db = mongojs(dbUrl, collections)
 
 module.exports = db
