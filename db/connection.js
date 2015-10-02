2ee78f93be1b293be0d46750b033a164523bd9d4
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-10-01 20:03:35 -0400

    updated connection for local hosting and testing

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,21 @@
 var mongojs = require("mongojs")
 
-// var dbUrl = "d3bq"
-// var collections = ["barbecues"]
-
-var mongoURI = process.env.MONGOLAB_URI;
-var localURI = 'mongodb://localhost/barbecues';
-var db = mongojs( mongoURI || localURI, ['barbecues']);
 
 
-// var db = mongojs(dbUrl, collections)
+// // mongojs for local hosting
+
+var dbUrl = "d3bq"
+var collections = ["barbecues"]
+var db = mongojs(dbUrl, collections)
+
+
+
+//  // mongojs for heroku use
+
+// var mongoURI = process.env.MONGOLAB_URI;
+// var localURI = 'mongodb://localhost/barbecues';
+// var db = mongojs( mongoURI || localURI, ['barbecues']);
+
+
 
 module.exports = db
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 ### d3bq
 
-Welcome to d3bq, the best companion app to any BBQ tour in the Southeastern U.S.A.
+Welcome to d3bq, the best companion app to any BBQ tour in the Southeastern U.S.
+
+## Requirements
+
+- [Node.js](http://nodejs.org/)
+- [Bower](http://bower.io/)
+
+## Running locally
+
+    $ git clone git@github.com:petergcutler/d3bq.git
+    $ cd d3bq
+    $ npm install
+    $ bower install
+    $ nodemon
