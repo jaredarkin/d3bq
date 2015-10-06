22bd6802620a19799f4a7f636d2c94016ea30fbd
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-10-05 22:06:54 -0400

    updated voronoi map call

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   console.log("jQ ready")
 
+
   // draw map
   L.mapbox.accessToken = 'pk.eyJ1IjoicGV0ZXJnY3V0bGVyIiwiYSI6ImNpZWt1MnczdDAwNDJzbW00OGhidXNnenEifQ.Zcv598pJc80mbyWueMSDmw';
   var map = L.mapbox.map('map', 'mapbox.outdoors', { zoomControl: false })
