c65f00871a831b3345047e910d804ba59f6e6295
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-10-05 21:37:54 -0400

    updates to authMechanism for heroku/mongoLab

--- db/connection.js
+++ db/connection.js
@@ -2,19 +2,19 @@ var mongojs = require("mongojs")
 
 
 
-// // mongojs for local hosting
+// // // mongojs for local hosting
+//
+// var dbUrl = "d3bq"
+// var collections = ["barbecues"]
+// var db = mongojs(dbUrl, collections)
 
-var dbUrl = "d3bq"
-var collections = ["barbecues"]
-var db = mongojs(dbUrl, collections)
 
 
+ // mongojs for heroku use
 
-//  // mongojs for heroku use
-
-// var mongoURI = process.env.MONGOLAB_URI;
-// var localURI = 'mongodb://localhost/barbecues';
-// var db = mongojs( mongoURI || localURI, ['barbecues']);
+var mongoURI = process.env.MONGOLAB_URI;
+var localURI = 'mongodb://localhost/barbecues';
+var db = mongojs( mongoURI || localURI, ['barbecues']);
 
 
 
