b82acd794579361a9b3dd8a17795d41e2cccbb6e
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-10-05 21:42:02 -0400

    updates to authMechanism for heroku/mongoLab

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,15 @@
 var mongojs = require("mongojs")
 
-
-
 // // // mongojs for local hosting
 //
 // var dbUrl = "d3bq"
 // var collections = ["barbecues"]
 // var db = mongojs(dbUrl, collections)
 
-
-
  // mongojs for heroku use
 
 var mongoURI = process.env.MONGOLAB_URI;
 var localURI = 'mongodb://localhost/barbecues';
-var db = mongojs( mongoURI || localURI, ['barbecues']);
-
-
+var db = mongojs( mongoURI || localURI, ['barbecues'], {authMechanism: 'ScramSHA1'});
 
 module.exports = db
